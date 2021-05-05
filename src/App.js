6340887a12b837403aa3a74536a013991e8d6231
GregTailor <szg0219@gmail.com>
2021-05-05 21:17:50 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./lul.png";
+import "./App.css";
+import Joke from "./components/joke";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Joke></Joke>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://sv443.net/jokeapi/v2/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          JokeAPI
         </a>
       </header>
     </div>
